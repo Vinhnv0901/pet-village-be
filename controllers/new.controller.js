b6a16d210931e439c159264b82895b2a5e80485e
Vinhnv0901 <vinhnv673019@gmail.com>
2024-04-05 11:48:27 +0700

    fix import controller

--- controllers/new.controller.js
+++ controllers/new.controller.js
@@ -28,7 +28,9 @@ const getNew = async (req, res) => {
 
 const createNew = async (req, res) => {
   try {
+    console.log("create a paper", req.body);
     const paper = await New.create(req.body);
+    console.log("create a paper");
     res.status(200).json(paper);
   } catch (error) {
     res.status(500).json({ message: error.message });
--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
-const { Product } = require("./product.model");
+const Product = require("./product.model");
 
-const { User } = require("./user.model");
+const User = require("./user.model");
 
-const { New } = require("./new.model");
+const New = require("./new.model");
 
-const { Order } = require("./order.model");
+const Order = require("./order.model");
 
 module.exports = { Order, Product, User, New };
